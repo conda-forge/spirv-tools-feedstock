ddf0508cc653d313d19e67270ffb9a99eca7c24a
Mark Harfouche <mark.harfouche@gmail.com>
2023-10-15 09:27:46 -0400

    Use conda-forge spirv-headers

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,4 @@
+set -ex
 mkdir build
 cd build
 
@@ -5,6 +6,7 @@ cmake ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DSPIRV_TOOLS_LIBRARY_TYPE=SHARED \
   -DSPIRV_TOOLS_BUILD_STATIC=OFF \
+  -DSPIRV_HEADER_INCLUDE_DIR="${PREFIX}/include" \
   ..
 
 make -j${CPU_COUNT}
